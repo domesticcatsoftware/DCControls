f3c6672c74a6335c4f7b37759eb0348957ad844f
Patrick Richards <pat@domesticcat.com.au>
2011-04-26 16:21:29 +1000

    Initial Commit

---
+++ DCControls/DCControlsAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  DCControlsAppDelegate.h
+//  DCControls
+//
+//  Created by Domestic Cat on 26/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DCControlsAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
